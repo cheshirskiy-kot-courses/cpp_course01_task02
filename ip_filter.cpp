7ab59f2c457b5bfa50139ec2837357eace713ed2
cheshirskiy-kot-courses <cheshirskiy-kot-courses@yandex.com>
2021-01-18 17:05:00 +0300

    add: solve task

--- ip_filter.cpp
+++ ip_filter.cpp
@@ -4,10 +4,22 @@
 #include "lib.h"
 
 
+void print_pool(const IP_Pool & ip_pool)
+{
+    for (auto & ip : ip_pool) {
+        std::cout << std::to_string(ip[0]);
+        for (int i = 1; i < ip.size(); i++) {
+            std::cout << "." << std::to_string(ip[i]);
+        }
+        std::cout << std::endl;
+    }
+}
+
+
 int main(int argc, char const * argv[])
 {
     try {
-        std::vector<IP> ip_pool;
+        IP_Pool ip_pool;
 
         for (std::string line; std::getline(std::cin, line);) {
             ip_pool.push_back(parse_ip(line));
@@ -16,13 +28,14 @@ int main(int argc, char const * argv[])
 
         rsort_lexicographically(ip_pool.begin(), ip_pool.end());
 
-        for (auto & ip : ip_pool) {
-            std::cout << std::to_string(ip[0]);
-            for (int i = 1; i < ip.size() ; i++) {
-                std::cout << "." << std::to_string(ip[i]);
-            }
-            std::cout << std::endl;
-        }
+        auto ip_pool_1 = filter(ip_pool, 1);
+        auto ip_pool_46_70 = filter(ip_pool, 46, 70);
+        auto ip_pool_any_46 = filter_any(ip_pool, 46);
+
+        print_pool(ip_pool);
+        print_pool(ip_pool_1);
+        print_pool(ip_pool_46_70);
+        print_pool(ip_pool_any_46);
     } catch (const std::exception & e) {
         std::cerr << e.what() << std::endl;
     }
